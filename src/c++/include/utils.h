01ec93c3f9372e0346456b5442d33b51c6cdd4c4
vanthuc2503 <thucpkbn@gmail.com>
2025-12-03 09:38:40 +0700

    Split utils to 2 files

--- src/c++/include/utils.h
+++ src/c++/include/utils.h
@@ -6,26 +6,8 @@
 #include <map>
 
 // Helper function: Take all unique object types from instances
-inline std::vector<std::string> getAllObjectTypes(const std::vector<SpatialInstance>& instances) {
-    std::set<std::string> objectTypesSet;
-    
-    for (const auto& instance : instances) {
-        std::string objectType = instance.id.substr(0, 1);
-        objectTypesSet.insert(objectType);
-    }
-    
-    return std::vector<std::string>(objectTypesSet.begin(), objectTypesSet.end());
-}
+std::vector<FeatureType> getAllObjectTypes(const std::vector<SpatialInstance>& instances);
 
 // Helper function: count the number of instances for each feature
-inline std::map<std::string, int> countInstancesByFeature(const std::vector<SpatialInstance>& instances) {
-    std::map<std::string, int> featureCount;
-    
-    for (const auto& instance : instances) {
-        std::string objectType = instance.id.substr(0, 1);
-        featureCount[objectType]++;
-    }
-    
-    return featureCount;
-}
+std::map<FeatureType, int> countInstancesByFeature(const std::vector<SpatialInstance>& instances);
 
---
+++ src/c++/src/utils.cpp
@@ -0,0 +1,27 @@
+#include "utils.h"
+#include <set>
+
+// Helper function: Take all unique object types from instances
+std::vector<FeatureType> getAllObjectTypes(const std::vector<SpatialInstance>& instances) {
+    std::set<FeatureType> objectTypesSet;
+    
+    for (const auto& instance : instances) {
+        FeatureType objectType = instance.id.substr(0, 1);
+        objectTypesSet.insert(objectType);
+    }
+    
+    return std::vector<FeatureType>(objectTypesSet.begin(), objectTypesSet.end());
+}
+
+// Helper function: count the number of instances for each feature
+std::map<FeatureType, int> countInstancesByFeature(const std::vector<SpatialInstance>& instances) {
+    std::map<FeatureType, int> featureCount;
+    
+    for (const auto& instance : instances) {
+        FeatureType objectType = instance.id.substr(0, 1);
+        featureCount[objectType]++;
+    }
+    
+    return featureCount;
+}
+
